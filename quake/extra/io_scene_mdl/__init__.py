c721aa4245254783b2f1efb118d8f39b2bed5a01
Joshua Skelton <joshua.skelton@gmail.com>
2017-02-11 17:15:56 -0800

    Initial Work on Blender Addon

---
+++ quake/extra/io_scene_mdl/__init__.py
@@ -0,0 +1,118 @@
+bl_info = {
+    "name": "Quake .MDL format",
+    "author": "Joshua Skelton",
+    "version": (0, 0, 1),
+    "blender": (2, 77, 0),
+    "location": "File > Import-Export",
+    "description": "Load a Quake MDL file.",
+    "warning": "",
+    "wiki_url": ""
+                "",
+    "support": 'COMMUNITY',
+    "category": "Import-Export"}
+
+if "bpy" in locals():
+    import importlib
+
+    if "import_mdl" in locals():
+        importlib.reload(import_mdl)
+    if "export_mdl" in locals():
+        importlib.reload(export_mdl)
+
+import bpy
+from bpy.props import StringProperty
+
+from bpy_extras.io_utils import (
+    ImportHelper,
+    ExportHelper,
+    path_reference_mode,
+)
+
+
+class ImportMDL(bpy.types.Operator, ImportHelper):
+    """Load a Quake MDL File"""
+    bl_idname = "import_scene.mdl"
+    bl_label = "Import MDL"
+    bl_options = {'UNDO'}
+
+    filename_ext = ".mdl"
+    filter_glob = StringProperty(
+        default="*.mdl",
+        options={'HIDDEN'},
+    )
+
+    def execute(self, context):
+        from . import import_mdl
+
+        return import_mdl.load(self, context, self.filepath)
+
+    def draw(self, context):
+        layout = self.layout
+
+        space = context.space_data
+
+        if space.system_bookmarks:
+            row = layout.row()
+            row.template_list("FILEBROWSER_UL_dir", "system_bookmarks", space,
+                              "system_bookmarks",
+                              space, "system_bookmarks_active",
+                              item_dyntip_propname="path", rows=1, maxrows=10)
+
+
+class ExportMDL(bpy.types.Operator, ExportHelper):
+    """Save a Quake MDL File"""
+
+    bl_idname = "export_scene.mdl"
+    bl_label = 'Export MDL'
+    bl_options = {'PRESET'}
+
+    filename_ext = ".mdl"
+    filter_glob = StringProperty(
+        default="*.mdl",
+        options={'HIDDEN'},
+    )
+
+    path_mode = path_reference_mode
+
+    check_extension = True
+
+    def execute(self, context):
+        from . import export_mdl
+
+        keywords = self.as_keywords(ignore=("axis_forward",
+                                            "axis_up",
+                                            "global_scale",
+                                            "check_existing",
+                                            "filter_glob",
+                                            "path_mode",
+                                            ))
+
+        return export_mdl.save(self, context, **keywords)
+
+
+def menu_func_import(self, context):
+    self.layout.operator(ImportMDL.bl_idname,
+                         text="Quake MDL (.mdl)")
+
+
+def menu_func_export(self, context):
+    self.layout.operator(ExportMDL.bl_idname,
+                         text="Quake MDL (.mdl)")
+
+
+def register():
+    bpy.utils.register_module(__name__)
+
+    bpy.types.INFO_MT_file_import.append(menu_func_import)
+    #bpy.types.INFO_MT_file_export.append(menu_func_export)
+
+
+def unregister():
+    bpy.utils.unregister_module(__name__)
+
+    bpy.types.INFO_MT_file_import.remove(menu_func_import)
+    #bpy.types.INFO_MT_file_export.remove(menu_func_export)
+
+
+if __name__ == "__main__":
+    register()
---
+++ quake/extra/io_scene_mdl/import_mdl.py
@@ -0,0 +1,70 @@
+import bpy
+import bmesh
+
+from .quake.mdl import Mdl, is_mdlfile
+
+def load(operator, context, filepath):
+
+    if not is_mdlfile(filepath):
+        # TODO: Error out
+        return { 'FINISHED' }
+
+    mdl = Mdl.open(filepath)
+    mdl.close()
+
+    # Load skin and create a material
+    image = mdl.image()
+    img = bpy.data.images.new('IMG', image.width, image.height)
+    pixels = list(map(lambda x: x / 255, image.pixels))
+
+    img.pixels[:] = pixels
+    img.update()
+
+    tex = bpy.data.textures.new('TEX', 'IMAGE')
+    tex.image = img
+
+    mat = bpy.data.materials.new('MAT')
+    mat.diffuse_color = 1,1,1
+
+    tex_slot = mat.texture_slots.add()
+    tex_slot.texture = tex
+    tex_slot.texture_coords = 'UV'
+
+    # Create mesh data
+    mesh = mdl.mesh(0)
+    me = bpy.data.meshes.new(mdl.frames[0].name)
+    bm = bmesh.new()
+
+    for vertex in mesh.vertices:
+        bm.verts.new(vertex)
+
+    bm.verts.ensure_lookup_table()
+    uv_layer = bm.loops.layers.uv.new()
+
+    for triangle in mesh.triangles:
+        t0 = bm.verts[triangle[0]]
+        t1 = bm.verts[triangle[1]]
+        t2 = bm.verts[triangle[2]]
+
+        face = bm.faces.new((t0, t1, t2))
+
+        face.loops[0][uv_layer].uv = mesh.uvs[triangle[0]]
+        face.loops[1][uv_layer].uv = mesh.uvs[triangle[1]]
+        face.loops[2][uv_layer].uv = mesh.uvs[triangle[2]]
+
+    bm.to_mesh(me)
+    bm.free()
+
+    ob = bpy.data.objects.new(mdl.frames[0].name, me)
+
+    ob.scale = mdl.scale
+    ob.location = mdl.origin
+
+    for texpoly in me.uv_textures[0].data:
+        texpoly.image = img
+
+    me.materials.append(mat)
+
+    bpy.context.scene.objects.link(ob)
+
+    return { 'FINISHED' }
