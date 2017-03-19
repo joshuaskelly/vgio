ba1181aa005c02665a0853815d3d18fa1b829453
Joshua Skelton <joshua.skelton@gmail.com>
2017-03-18 20:21:49 -0700

    Initial Implementation of bsp2wad

---
+++ quake/utils/bsp2wad.py
@@ -0,0 +1,72 @@
+"""Command line utility for creating and creating WAD files from BSP files
+
+Supported Games:
+    - QUAKE
+"""
+
+import argparse
+import io
+import os
+import sys
+
+from quake import bsp, wad
+
+
+class ResolvePathAction(argparse.Action):
+    def __call__(self, parser, namespace, values, option_string=None):
+        if isinstance(values, list):
+            fullpath = [os.path.expanduser(v) for v in values]
+        else:
+            fullpath = os.path.expanduser(values)
+
+        setattr(namespace, self.dest, fullpath)
+
+
+class Parser(argparse.ArgumentParser):
+    """Simple wrapper class to provide help on error"""
+    def error(self, message):
+        sys.stderr.write('error: %s\n' % message)
+        self.print_help()
+        sys.exit(1)
+
+
+parser = Parser(prog='bsp2wad',
+                description='Default action is to create a wad archive from '
+                            'miptextures extracted from the file.',
+                epilog='example: bsp2wad {0} => creates the wad file {1}'.format('e1m1.bsp', 'e1m1.wad'))
+
+parser.add_argument('file', metavar='file.bsp', action=ResolvePathAction, help='wad file to ')
+parser.add_argument('-q', dest='quiet', action='store_true', help='quiet mode')
+
+args = parser.parse_args()
+
+print(args.file)
+
+if not bsp.is_bspfile(args.file):
+    print('{0}: cannot find or open {1}'.format(parser.prog, args.file),
+          file=sys.stderr)
+    sys.exit(1)
+
+bsp_file = bsp.Bsp.open(args.file)
+wad_path = os.path.dirname(args.file)
+wad_name = os.path.basename(args.file).split('.')[0] + '.wad'
+fullpath = os.path.join(wad_path, wad_name)
+
+with wad.WadFile(fullpath, mode='w') as wad_file:
+    for miptex in bsp_file.miptextures:
+        buff = io.BytesIO()
+        bsp.Miptexture.write(buff, miptex)
+        buff.seek(0)
+
+        info = wad.WadInfo(miptex.name)
+        info.file_size = 40 + len(miptex.pixels)
+        info.disk_size = info.file_size
+        info.compression = wad.CMP_NONE
+        info.type = wad.TYPE_MIPTEX
+
+        if not args.quiet:
+            print(' adding: %s' % info.filename)
+
+        wad_file.write_info(info, buff)
+
+sys.exit(0)
