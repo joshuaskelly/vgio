4751a6f32e373f55c74ee3fdc35afd2c2f1018eb
Joshua Skelton <joshua.skelton@gmail.com>
2017-04-21 22:09:22 -0700

    Removing Test App

--- test.py
+++
@@ -1,6 +0,0 @@
-import os
-
-for root, dirs, files in os.walk(os.path.expanduser('~/Downloads/qtest1')):
-    for name in files:
-        fullpath = os.path.join(root, name)
-        print(fullpath)
