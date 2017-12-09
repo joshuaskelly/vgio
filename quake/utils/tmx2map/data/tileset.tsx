2cbcd1572cff96c78183f278021126156efef736
Joshua Skelton <joshua.skelton@gmail.com>
2017-12-08 16:10:36 -0800

    Removed tmx2map util

--- quake/utils/tmx2map/data/tileset.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset name="tileset" tilewidth="32" tileheight="32" tilecount="16" columns="4">
- <image source="tileset.png" width="128" height="128"/>
-</tileset>
