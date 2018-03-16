1146b433b45c82560e129d042f811fba1ae90cad
Joshua Skelton <joshua.skelton@gmail.com>
2018-03-15 21:12:15 -0700

    Stubbing out Dmo format

---
+++ duke3d/duke3d/dmo.py
@@ -0,0 +1,10 @@
+"""This module provides file I/O for Duke3D Dmo files.
+
+Example:
+    demo_file = dmo.Dmo.open('demo1.dmo')
+
+References:
+    "DMO format"
+    - Paul Bourke
+    - http://paulbourke.net/dataformats/dmo/
+"""
